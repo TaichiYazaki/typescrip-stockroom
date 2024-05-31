81557fd01112dc08dc98f58de242c4eb361dcea2
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2024-06-01 08:38:57 +0900

    cloudfront追加

--- lib/typescript-stockroom-stack.ts
+++ lib/typescript-stockroom-stack.ts
@@ -1,16 +1,73 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as deployment from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TypescriptStockroomStack extends cdk.Stack {
+  // public readonly distribution: cloudfront.IDistribution;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3
+    const websiteBucket = new s3.Bucket(this, 'CreateWebsite', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TypescriptStockroomQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // OAC
+    const cfnOriginAccessControll = new cloudfront.CfnOriginAccessControl(
+      this,
+      'OriginAccessControl',
+      {
+        originAccessControlConfig: {
+          name: 'OriginAccessControlForContentsBucket',
+          originAccessControlOriginType: 's3',
+          signingBehavior: 'always',
+          signingProtocol: 'sigv4',
+          description: 'Access Control',
+        },
+      }
+    );
+    // cloudfront
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+      },
+      httpVersion: cdk.aws_cloudfront.HttpVersion.HTTP2_AND_3,
+      defaultRootObject: 'index.html',
+    });
+    const cfnDistribution = distribution.node
+      .defaultChild as cloudfront.CfnDistribution;
+    // OAI削除（勝手に設定されるため）
+    cfnDistribution.addPropertyOverride(
+      'DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity',
+      ''
+    );
+    // OAC設定
+    cfnDistribution.addPropertyOverride(
+      'DistributionConfig.Origins.0.OriginAccessControlId',
+      cfnOriginAccessControll.attrId
+    );
+
+    const webSiteBucketPolicyStatement = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      resources: [`${websiteBucket.bucketArn}/*`],
+    });
+
+    websiteBucket.addToResourcePolicy(webSiteBucketPolicyStatement);
+
+    new deployment.BucketDeployment(this, 'WebsiteDeploy', {
+      sources: [deployment.Source.asset('./website')],
+      destinationBucket: websiteBucket,
+    });
   }
 }
